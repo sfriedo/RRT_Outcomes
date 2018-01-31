3258df5f058e5143ab2dff595a9901ca963a8746
Siegfried Horschig <siegfried.horschig@student.hpi.de>
2018-01-31 22:07:10 +0100

    Use logging, add regression evaluation method

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 from knnimpute import knn_impute_few_observed
 from os import makedirs
 from os.path import exists
+import logging
 import sys
 sys.path.append('../sklearn-expertsys-master')
 from RuleListClassifier import RuleListClassifier
@@ -68,14 +69,14 @@ def get_data():
             result = pd.DataFrame.from_records(conn.fetchall(),
                                                columns=ATTRIBUTES)
         except Exception as e:
-            print(e)
+            logging.info(e)
     return result
 
 
 def process_data(df, target_name, scale=True):
-    print('Preparing data.')
+    logging.info('Preparing data.')
     # Extract target_name attribute
-    print('Encoding attributes.')
+    logging.info('Encoding attributes.')
     le = preprocessing.LabelEncoder()
     numeric_cols = list(set(ATTRIBUTES) - set(STRING_ATRRIBUTES))
     df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
@@ -89,7 +90,7 @@ def process_data(df, target_name, scale=True):
     df['VENT_DAYS_LESS_SEVEN'] = np.where(df['VENT_FREE_DAYS'] < 7, 1, 0)
     df = df.drop('VENT_FREE_DAYS', axis=1)
     # Impute missing values
-    print('Imputing missing data.')
+    logging.info('Imputing missing data.')
     df[:] = knn_impute_few_observed(df.as_matrix(),
                                     np.isnan(df.as_matrix()), k=3)
     # Normalize values to [0, 1]
@@ -98,7 +99,7 @@ def process_data(df, target_name, scale=True):
     for t in TARGET_ATTRIBUTES:
         df = df.drop(t, axis=1)
     if scale:
-        print('Scaling data.')
+        logging.info('Scaling data.')
         df[:] = preprocessing.MinMaxScaler().fit_transform(df)
     return df, target
 
@@ -106,16 +107,19 @@ def process_data(df, target_name, scale=True):
 def eval_clf_model(actual, pred):
     conf_matrix = confusion_matrix(actual, pred).ravel()
     tn, fp, fn, tp = conf_matrix
-    print('Confusion matrix: ', conf_matrix)
-    print('Recall (Sensitivity): ', tp / (tp + fn))
-    print('Specificity: ', tn / (tn + fp))
-    print('Precision: ', tp / (tp + fp))
+    logging.info('Confusion matrix: ' + str(conf_matrix))
+    logging.info('Recall (Sensitivity): ' + str(tp / (tp + fn)))
+    logging.info('Specificity: ' + str(tn / (tn + fp)))
+    logging.info('Precision: ' + str(tp / (tp + fp)))
 
 
 def eval_reg_model(actual, pred):
-    print('Explained Variance (R2): ', explained_variance_score(actual, pred))
-    print('Mean Squared Error: ', mean_squared_error(actual, pred))
-    print('Mean Absolute Error: ', mean_absolute_error(actual, pred))
+    logging.info('Explained Variance (R2): ' + str(
+        explained_variance_score(actual, pred)))
+    logging.info('Mean Squared Error: ' + str(
+        mean_squared_error(actual, pred)))
+    logging.info('Mean Absolute Error: ' + str(
+        mean_absolute_error(actual, pred)))
 
 
 def most_informative_feature_for_binary_classification(feature_names,
@@ -124,7 +128,7 @@ def most_informative_feature_for_binary_classification(feature_names,
     topn_class1 = sorted(zip(classifier.coef_[0], feature_names))[:n]
 
     for coef, feat in topn_class1:
-        print(class_labels[0], coef, feat)
+        logging.info(class_labels[0], coef, feat)
 
 
 def plot_roc(y_test, y_score, target_name, clf_name):
@@ -147,53 +151,74 @@ def plot_roc(y_test, y_score, target_name, clf_name):
     plt.clf()
 
 
+def plot_most_important_reg_features(coefs, target_name, num_feats=5):
+    coefs = sorted(coefs, key=lambda x: x[0])
+    coefs_top = coefs[:num_feats] + coefs[-num_feats:]
+    plt.figure(figsize=(14, 5))
+    plt.bar(range(len(coefs_top)), [x[0] for x in coefs_top])
+    plt.xticks(range(len(coefs_top)), [x[1] for x in coefs_top], fontsize=8)
+    if not exists(PLOT_DIR):
+        makedirs(PLOT_DIR)
+    plt.savefig(PLOT_DIR + 'coef_{}'.format(target_name))
+    plt.clf()
+
+
 def main():
+    logging.basicConfig(filename='logfile.log', level=logging.INFO)
     df_ori = get_data()
 
     for target_name in TARGET_ATTRIBUTES:
-        print('Training for {}'.format(target_name))
+        logging.info('Training for {}'.format(target_name))
 
-        # df, target = process_data(df_ori, target_name, False)
-        # print('Training bayesian rule lists.')
-        # brl = RuleListClassifier(class1label=target_name,
-        #                          verbose=False)
-        # y_pred = cross_val_predict(brl, df.as_matrix(), target.as_matrix(),
-        #                            cv=5)
-        # print('Accuracy: {}'.format(accuracy_score(target, y_pred)))
-        # eval_clf_model(target, y_pred)
-        # brl.fit(df.as_matrix(), target.as_matrix(),
-        #         feature_labels=df.columns)
-        # print(brl)
+        logging.info('Training bayesian rule lists.')
+        df, target = process_data(df_ori, target_name, False)
+        logging.info('Training cv.')
+        brl = RuleListClassifier(class1label=target_name,
+                                 verbose=False)
+        y_pred = cross_val_predict(brl, df.as_matrix(), target.as_matrix(),
+                                   cv=5)
+        logging.info('Accuracy: {}'.format(accuracy_score(target, y_pred)))
+        eval_clf_model(target, y_pred)
 
-        print('Training neural network.')
+        logging.info('Training neural network.')
         df, target = process_data(df_ori, target_name, True)
 
-        print('Training cv.')
+        logging.info('Training cv.')
         clf = MLPClassifier(activation='relu',
                             solver='adam', alpha=1e-5,
                             hidden_layer_sizes=(5, 2), random_state=1)
         y_pred = cross_val_predict(clf, df, target, cv=5)
-        print('Accuracy: {}'.format(accuracy_score(target, y_pred)))
+        logging.info('Accuracy: {}'.format(accuracy_score(target, y_pred)))
         eval_clf_model(target, y_pred)
 
-        print('Training train/test split.')
+        logging.info('Training MLP on train/test split.')
         y_pred = [0]
         while len([x for x in y_pred if x == 1.0]) < 1:
-            X_train, X_test, y_train, y_test = train_test_split(df, target,
-                                                                test_size=.3,
-                                                                stratify=target)
+            X_train, X_test, \
+                y_train, y_test = train_test_split(df, target,
+                                                   test_size=.3,
+                                                   stratify=target)
             clf.fit(X_train, y_train)
             y_pred = clf.predict(X_test)
         eval_clf_model(y_test, y_pred)
         y_pred = [x[1] for x in clf.predict_proba(X_test)]
         plot_roc(y_test, y_pred, target_name, 'MLPClassifier')
 
-        print('Training bayesian ridge regression as mimic model.')
+        logging.info('Training BRL on train/test split.')
+        brl.fit(X_train.as_matrix(), y_train.as_matrix())
+        y_pred = brl.predict(X_test.as_matrix())
+        eval_clf_model(y_test, y_pred)
+        y_pred = [x[1] for x in brl.predict_proba(X_test.as_matrix())]
+        plot_roc(y_test, y_pred, target_name, 'BRL')
+
+        logging.info('Training bayesian ridge regression as mimic model.')
         y_train = [x[1] for x in clf.predict_proba(X_train)]
         bayes = BayesianRidge()
         y_pred = bayes.fit(X_train, y_train).predict(X_test)
-        print(zip(bayes.coef_, df.columns))
+        logging.info(zip(bayes.coef_, df.columns))
         eval_reg_model(y_test, y_pred)
+        plot_most_important_reg_features(zip(bayes.coef_, df.columns),
+                                         target_name)
         plot_roc(y_test, y_pred, target_name, 'BayesianRidge')
 
 
